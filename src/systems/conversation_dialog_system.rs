4298b547179a4fec642e1b8d0ee278d33b2d7dec
vektorprime <vektorprime@gmail.com>
2026-02-11 21:07:19 -0600

    npc dialog working

--- src/systems/conversation_dialog_system.rs
+++ src/systems/conversation_dialog_system.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use bevy::{
+    math::Vec3Swizzles,
     prelude::{Assets, Entity, EventReader, Local, Query, Res, With},
 };
 use bevy_egui::{egui, EguiContexts};
@@ -14,7 +15,7 @@ use crate::{
     scripting::{
         lua4::{Lua4Function, Lua4VM, Lua4VMError, Lua4VMRustClosures, Lua4Value},
         LuaGameConstants, LuaGameFunctions, LuaQuestFunctions, LuaUserValueEntity,
-        ScriptFunctionResources,
+        ScriptFunctionContext, ScriptFunctionResources,
     },
     ui::{widgets::Dialog, DataBindings, DialogInstance},
     VfsResource,
@@ -41,15 +42,15 @@ pub struct ConversationDialogState {
     pub event_object_handle: Arc<dyn std::any::Any + Send + Sync>,
 }
 
-pub struct LuaVMContext<'a, 'w, 's> {
-    pub function_context: &'a mut crate::scripting::ScriptFunctionContext<'w, 's>,
-    pub function_resources: &'a ScriptFunctionResources<'w, 's>,
+pub struct LuaVMContext<'a, 'w1, 's1, 'w2, 's2> {
+    pub function_context: &'a mut crate::scripting::ScriptFunctionContext<'w1, 's1>,
+    pub function_resources: &'a ScriptFunctionResources<'w2, 's2>,
     pub game_constants: &'a LuaGameConstants,
     pub game_functions: &'a LuaGameFunctions,
     pub quest_functions: &'a LuaQuestFunctions,
 }
 
-impl<'a, 'w, 's> Lua4VMRustClosures for LuaVMContext<'a, 'w, 's> {
+impl<'a, 'w1, 's1, 'w2, 's2> Lua4VMRustClosures for LuaVMContext<'a, 'w1, 's1, 'w2, 's2> {
     fn call_rust_closure(
         &mut self,
         name: &str,
@@ -367,6 +368,7 @@ pub fn conversation_dialog_system(
     mut current_dialog_state: Local<Option<ConversationDialogState>>,
     mut egui_context: EguiContexts,
     mut conversation_dialog_events: EventReader<ConversationDialogEvent>,
+    mut lua_function_context: ScriptFunctionContext,
     mut ui_state: Local<UiConversationDialogState>,
     script_function_resources: ScriptFunctionResources,
     query_player_position: Query<&Position, With<PlayerCharacter>>,
@@ -407,8 +409,318 @@ pub fn conversation_dialog_system(
     }
     let dialog_sprites = ui_state.sprites.as_ref().unwrap();
 
-    // TODO: Fix ScriptFunctionContext and LuaVMContext usage for Bevy 0.13
-    // The system is currently disabled due to complex lifetime issues with ScriptFunctionContext
-    // This needs to be refactored to work with Bevy 0.13's SystemParam requirements
-    return;
+    let mut user_context = LuaVMContext {
+        function_context: &mut lua_function_context,
+        function_resources: &script_function_resources,
+        game_constants: &lua_game_constants,
+        game_functions: &lua_game_functions,
+        quest_functions: &lua_quest_functions,
+    };
+
+    // Process incoming events to open dialogs
+    for event in conversation_dialog_events.read() {
+        let (owner_entity, con_file_path) = match event {
+            ConversationDialogEvent::OpenNpcDialog(npc_entity, con_file_path) => {
+                (Some(*npc_entity), con_file_path)
+            }
+            ConversationDialogEvent::OpenEventDialog(con_file_path) => (None, con_file_path),
+        };
+        *current_dialog_state = None;
+
+        if let Some(mut next_dialog_state) = vfs_resource
+            .vfs
+            .read_file::<ConFile, _>(con_file_path)
+            .ok()
+            .and_then(|con_file| {
+                create_conversation_dialog(con_file, &mut user_context, owner_entity)
+            })
+        {
+            let check_open_function =
+                &next_dialog_state.con_file.initial_messages[0].condition_function;
+
+            if !check_open_function.is_empty() {
+                match next_dialog_state.lua_vm.call_global_closure(
+                    &mut user_context,
+                    check_open_function,
+                    &[Lua4Value::UserData(
+                        next_dialog_state.event_object_handle.clone(),
+                    )],
+                ) {
+                    Ok(result) => {
+                        let result = result
+                            .get(0)
+                            .and_then(|value| value.to_i32().ok())
+                            .unwrap_or(0);
+                        if result < 1 {
+                            log::trace!(target: "con",
+                                "Conversation check open function {} failed with value {}",
+                                check_open_function,
+                                result
+                            );
+                        } else {
+                            // Success, open dialog
+                            if next_dialog_state.generated_dialog.run_menu(
+                                &mut next_dialog_state.lua_vm,
+                                &mut user_context,
+                                &next_dialog_state.con_file,
+                                &next_dialog_state.event_object_handle,
+                                &game_data,
+                                0,
+                            ) {
+                                *current_dialog_state = Some(next_dialog_state);
+                            }
+                        }
+                    }
+                    Err(error) => {
+                        log::error!(target: "con",
+                            "Error running conversation open script function {}: {}",
+                            check_open_function,
+                            error
+                        );
+                    }
+                }
+            } else {
+                // No check function, just open the dialog
+                if next_dialog_state.generated_dialog.run_menu(
+                    &mut next_dialog_state.lua_vm,
+                    &mut user_context,
+                    &next_dialog_state.con_file,
+                    &next_dialog_state.event_object_handle,
+                    &game_data,
+                    0,
+                ) {
+                    *current_dialog_state = Some(next_dialog_state);
+                }
+            }
+        }
+    }
+
+    // Render the dialog if one is active
+    if let Some(dialog_state) = current_dialog_state.as_mut() {
+        let mut selected_response = None;
+        let mut open = true;
+
+        // If player has moved away from NPC, close the dialog
+        if let (Ok(player_position), Some(npc_position)) = (
+            query_player_position.get_single(),
+            dialog_state
+                .owner_entity
+                .and_then(|entity| query_position.get(entity).ok()),
+        ) {
+            if npc_position.position.xy().distance(player_position.xy()) > 400.0 {
+                *current_dialog_state = None;
+                return;
+            }
+        }
+
+        let (message_galley, num_message_middle, num_response_middles) =
+            egui_context.ctx_mut().fonts(|fonts| {
+                let message_galley =
+                    fonts.layout_job(dialog_state.generated_dialog.message.clone());
+
+                let message_size = message_galley.size();
+                let num_message_middle =
+                    ((message_size.y + dialog_sprites.message_middle.height - 1.0)
+                        / dialog_sprites.message_middle.height) as usize;
+
+                let mut response_size_y =
+                    (dialog_state.generated_dialog.responses.len().max(1) - 1) as f32 * 5.0;
+                for response in dialog_state.generated_dialog.responses.iter_mut() {
+                    let galley = fonts.layout_job(response.text.clone());
+                    response_size_y += galley.size().y;
+                    response.galley = Some(galley);
+                }
+                let num_response_middles =
+                    ((response_size_y + dialog_sprites.answer_middle.height - 1.0)
+                        / dialog_sprites.answer_middle.height) as usize;
+
+                (message_galley, num_message_middle, num_response_middles)
+            });
+
+        let dialog_height = dialog.height
+            + dialog_sprites.message_top.height
+            + num_message_middle as f32 * dialog_sprites.message_middle.height
+            + dialog_sprites.message_bottom.height
+            + dialog_sprites.answer_top.height
+            + num_response_middles as f32 * dialog_sprites.answer_middle.height
+            + dialog_sprites.answer_bottom.height;
+
+        let title = dialog_state
+            .owner_entity
+            .and_then(|entity| query_name.get(entity).ok())
+            .map(|name| name.as_str())
+            .unwrap_or("Event Dialog");
+
+        let mut response_close_button = None;
+        let screen_size = egui_context
+            .ctx_mut()
+            .input(|input| input.screen_rect().size());
+        let default_x = screen_size.x / 2.0 - dialog.width / 2.0;
+        let default_y = screen_size.y / 2.0 - dialog_height / 2.0;
+
+        egui::Window::new("Conversation Dialog")
+            .frame(egui::Frame::none())
+            .open(&mut open)
+            .title_bar(false)
+            .resizable(false)
+            .min_width(dialog.width)
+            .min_height(dialog_height)
+            .default_pos([default_x, default_y])
+            .show(egui_context.ctx_mut(), |ui| {
+                dialog.draw(
+                    ui,
+                    DataBindings {
+                        response: &mut [(10, &mut response_close_button)],
+                        ..Default::default()
+                    },
+                    |ui, _bindings| {
+                        ui.put(
+                            egui::Rect::from_min_size(ui.min_rect().min, egui::vec2(350.0, 20.0)),
+                            egui::Label::new(
+                                egui::RichText::new(title).color(egui::Color32::WHITE).font(
+                                    egui::FontId::new(
+                                        14.0,
+                                        egui::FontFamily::Name("Ubuntu-M".into()),
+                                    ),
+                                ),
+                            ),
+                        );
+
+                        let mut pos = ui.min_rect().min + egui::vec2(0.0, 24.0);
+                        dialog_sprites.message_top.draw(ui, pos);
+                        pos.y += dialog_sprites.message_top.height - 1.0;
+
+                        let message_rect_min = pos;
+                        for _ in 0..num_message_middle {
+                            dialog_sprites.message_middle.draw(ui, pos);
+                            pos.y += dialog_sprites.message_middle.height - 1.0;
+                        }
+
+                        dialog_sprites.message_bottom.draw(ui, pos);
+                        pos.y += dialog_sprites.message_bottom.height - 1.0;
+
+                        dialog_sprites.answer_top.draw(ui, pos);
+                        pos.y += dialog_sprites.answer_top.height - 1.0;
+
+                        let response_rect_min = pos;
+                        for _ in 0..num_response_middles {
+                            dialog_sprites.answer_middle.draw(ui, pos);
+                            pos.y += dialog_sprites.answer_middle.height - 1.0;
+                        }
+
+                        dialog_sprites.answer_bottom.draw(ui, pos);
+                        pos.y += dialog_sprites.answer_bottom.height;
+
+                        let message_rect = egui::Rect::from_min_max(
+                            message_rect_min + egui::vec2(25.0, 0.0),
+                            egui::pos2(message_rect_min.x + 25.0 + 300.0, pos.y),
+                        );
+                        ui.allocate_ui_at_rect(message_rect, |ui| {
+                            ui.horizontal_top(|ui| ui.add(egui::Label::new(message_galley.clone())))
+                        });
+
+                        let mut response_rect_min = response_rect_min + egui::vec2(15.0, 5.0);
+                        for (index, answer) in
+                            dialog_state.generated_dialog.responses.iter().enumerate()
+                        {
+                            let answer_galley = answer.galley.clone().unwrap();
+                            let line_rect =
+                                egui::Rect::from_min_size(response_rect_min, answer_galley.size());
+
+                            let response = ui.allocate_rect(line_rect, egui::Sense::click());
+                            if ui.is_rect_visible(response.rect) {
+                                ui.painter().add(egui::epaint::TextShape {
+                                    pos: response.rect.left_top(),
+                                    galley: answer_galley,
+                                    override_text_color: if response.hovered() {
+                                        Some(egui::Color32::BLUE)
+                                    } else {
+                                        None
+                                    },
+                                    underline: egui::Stroke::NONE,
+                                    angle: 0.0,
+                                    fallback_color: egui::Color32::BLACK,
+                                    opacity_factor: 1.0,
+                                });
+                            }
+
+                            if response.clicked() {
+                                selected_response = Some(index);
+                            }
+
+                            response_rect_min.y += line_rect.height() + 5.0;
+                        }
+                    },
+                );
+            });
+
+        if response_close_button.map_or(false, |r| r.clicked()) {
+            open = false;
+        }
+
+        if !open {
+            // User closed the dialog
+            *current_dialog_state = None;
+            return;
+        }
+
+        if selected_response.is_none() {
+            egui_context.ctx_mut().input(|input| {
+                for (index, &key) in [
+                    egui::Key::Num1,
+                    egui::Key::Num2,
+                    egui::Key::Num3,
+                    egui::Key::Num4,
+                    egui::Key::Num5,
+                    egui::Key::Num6,
+                    egui::Key::Num7,
+                    egui::Key::Num8,
+                    egui::Key::Num9,
+                ]
+                .iter()
+                .enumerate()
+                {
+                    if input.key_pressed(key) {
+                        selected_response = Some(index);
+                    }
+                }
+            });
+        }
+
+        if let Some(selected_response) = selected_response {
+            if let Some(selected_response) = dialog_state
+                .generated_dialog
+                .responses
+                .get(selected_response)
+            {
+                let click_action_function = &selected_response.action_function;
+                if !click_action_function.is_empty() {
+                    if let Err(error) = dialog_state.lua_vm.call_global_closure(
+                        &mut user_context,
+                        click_action_function,
+                        &[Lua4Value::UserData(
+                            dialog_state.event_object_handle.clone(),
+                        )],
+                    ) {
+                        log::error!(
+                            "Error running conversation click action function {}: {}",
+                            click_action_function,
+                            error
+                        );
+                    }
+                }
+
+                if !dialog_state.generated_dialog.run_menu(
+                    &mut dialog_state.lua_vm,
+                    &mut user_context,
+                    &dialog_state.con_file,
+                    &dialog_state.event_object_handle,
+                    &game_data,
+                    selected_response.menu_index,
+                ) {
+                    *current_dialog_state = None;
+                }
+            }
+        }
+    }
 }
