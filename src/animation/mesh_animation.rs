2269b9828e671a5ee497141a4e756b4c9c7cbea8
vektorprime <vektorprime@gmail.com>
2026-08-04 21:51:56 -0500

    mesh animation: precomputed ZMO flags + uniform early-out before material mutation (01-F4)

--- src/animation/mesh_animation.rs
+++ src/animation/mesh_animation.rs
@@ -11,11 +11,7 @@ use bevy::{
 use crate::{
     animation::{AnimationState, ZmoAsset},
     components::EffectMesh,
-    render::{
-        EffectMeshAnimationUniform, RoseEffectExtension, EFFECT_MESH_ANIMATION_FLAG_ALPHA,
-        EFFECT_MESH_ANIMATION_FLAG_NORMAL, EFFECT_MESH_ANIMATION_FLAG_POSITION,
-        EFFECT_MESH_ANIMATION_FLAG_UV,
-    },
+    render::{EffectMeshAnimationUniform, RoseEffectExtension},
 };
 
 #[derive(Component, Reflect, Deref, DerefMut)]
@@ -77,14 +73,28 @@ pub fn mesh_animation_system(
 
         // If this entity has an effect mesh material, update its animation uniform
         if let Some(material_handle) = material_component.map(|m| m.0.clone()) {
-            if let Some(material) = effect_mesh_materials.get_mut(&material_handle) {
+            if let Some(material) = effect_mesh_materials.get(&material_handle) {
                 // Only update if there's an animation texture present
                 if material.extension.animation_texture.is_some() {
-                    update_effect_mesh_animation_material(
-                        &mut material.extension.animation_state,
-                        zmo_asset,
-                        anim_state,
-                    );
+                    let uniform = &material.extension.animation_state;
+                    let current_frame = anim_state.current_frame_index() as u32 & 0xFFFF;
+                    let next_frame = anim_state.next_frame_index() as u32 & 0xFFFF;
+                    let current_next_frame = current_frame | (next_frame << 16);
+                    let next_weight = anim_state.current_frame_fract();
+                    if uniform.flags != zmo_asset.flags
+                        || uniform.current_next_frame != current_next_frame
+                        || uniform.next_weight != next_weight
+                    {
+                        if let Some(material) = effect_mesh_materials.get_mut(&material_handle) {
+                            update_effect_mesh_animation_material(
+                                &mut material.extension.animation_state,
+                                zmo_asset,
+                                anim_state,
+                                current_next_frame,
+                                next_weight,
+                            );
+                        }
+                    }
                 }
             }
         }
@@ -96,38 +106,18 @@ fn update_effect_mesh_animation_material(
     uniform: &mut EffectMeshAnimationUniform,
     zmo_asset: &ZmoAsset,
     anim_state: &AnimationState,
+    current_next_frame: u32,
+    next_weight: f32,
 ) {
-    // Build flags: bits 0-3 = animation type flags, bits 4-31 = num_frames
-    let mut flags: u32 = 0;
+    uniform.flags = zmo_asset.flags;
+    uniform.current_next_frame = current_next_frame;
+    uniform.next_weight = next_weight;
+
+    // Get alpha value for the current frame if available
     if let Some(texture_data) = &zmo_asset.animation_texture {
-        if texture_data.has_position_channel {
-            flags |= EFFECT_MESH_ANIMATION_FLAG_POSITION;
-        }
-        if texture_data.has_normal_channel {
-            flags |= EFFECT_MESH_ANIMATION_FLAG_NORMAL;
-        }
-        if texture_data.has_uv1_channel {
-            flags |= EFFECT_MESH_ANIMATION_FLAG_UV;
-        }
-        if texture_data.has_alpha_channel {
-            flags |= EFFECT_MESH_ANIMATION_FLAG_ALPHA;
-        }
-        // Get alpha value for current frame if available
         let current_frame = anim_state.current_frame_index();
         if let Some(alpha) = texture_data.alphas.get(current_frame).copied() {
             uniform.alpha = alpha;
         }
     }
-    // Pack num_frames into upper bits (bits 4-31)
-    flags |= (zmo_asset.num_frames as u32) << 4;
-
-    uniform.flags = flags;
-
-    // Pack current and next frame indices: lower 16 bits = current, upper 16 bits = next
-    let current_frame = anim_state.current_frame_index() as u32 & 0xFFFF;
-    let next_frame = anim_state.next_frame_index() as u32 & 0xFFFF;
-    uniform.current_next_frame = current_frame | (next_frame << 16);
-
-    // Set interpolation weight between frames
-    uniform.next_weight = anim_state.current_frame_fract();
 }
